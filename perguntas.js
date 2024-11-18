b8e985278af3a4826a9645a2af60a2e7dfd419dd
henrysilvaaa <00001101364786sp@al.educacao.sp.gov.br>
2024-11-18 11:49:45 -0300

    flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O java é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz cavalo em Inglês?',
+    'Oi em ingles é horse (rorse)'
+)
